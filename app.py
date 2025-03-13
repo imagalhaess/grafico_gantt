01baa810be761cd13d336d13e7caccd6bd5dc5a5
Isabela Magalhães <magalhaes569@gmail.com>
2025-03-13 18:17:25 -0300

    Novas mudanças

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import pandas as pd
+
+url = "https://github.com/imagalhaess/grafico_gantt.git"
+df = pd.read_excel(url, header=0)
 
 # ⬇️ Carregar dados do cronograma
 caminho_arquivo = "dados/cronograma.xlsx"
